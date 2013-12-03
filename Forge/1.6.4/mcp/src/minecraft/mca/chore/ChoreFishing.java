9ca4231d5d114a3b7f99a73efa951896e760740e
WildBamaBoy <dustya@otelco.net>
2013-12-03 16:39:33 -0600

    Refactored fishing chore

--- Forge/1.6.4/mcp/src/minecraft/mca/chore/ChoreFishing.java
+++ Forge/1.6.4/mcp/src/minecraft/mca/chore/ChoreFishing.java
@@ -34,34 +34,34 @@ import cpw.mods.fml.common.network.PacketDispatcher;
 public class ChoreFishing extends AbstractChore
 {
 	/** An instance of the fish hook entity.*/
-	public transient EntityChoreFishHook fishEntity = null;
+	public transient EntityChoreFishHook fishEntity;
 
 	/** Does the owner have coordinates of water?*/
-	public boolean hasWaterCoordinates = false;
+	public boolean hasWaterCoordinates;
 
 	/** Does the owner have a random water block they should move to?*/
-	public boolean hasRandomWaterBlock = false;
+	public boolean hasFishingTarget;
 
 	/** Is the owner currently fishing?*/
-	public boolean isFishing = false;
+	public boolean isFishing;
 
 	/** The X coordinate of the current water block.*/
-	public int waterCoordinatesX = 0;
+	public int waterCoordinatesX;
 
 	/** The Y coordinate of the current water block.*/
-	public int waterCoordinatesY = 0;
+	public int waterCoordinatesY;
 
 	/** The Z coordinate of the current water block.*/
-	public int waterCoordinatesZ = 0;
+	public int waterCoordinatesZ;
 
 	/** How many ticks have passed since the fish hook has been thrown.*/
-	public int fishingTicks = 0;
+	public int fishingTicks;
 
 	/** The amount of ticks at which the owner will have a chance to catch a fish.*/
-	public int nextFishCatchChance = 0;
+	public int fishCatchCheck;
 
 	/** How many ticks the owner has remained idle, with no fish hook thrown.*/
-	public int idleFishingTicks = 0;
+	public int idleFishingTime;
 
 	/**
 	 * Constructor
@@ -76,16 +76,14 @@ public class ChoreFishing extends AbstractChore
 	@Override
 	public void beginChore()
 	{
-		if (MCA.getInstance().isDedicatedServer)
+		if (MCA.getInstance().isDedicatedServer && !MCA.getInstance().modPropertiesManager.modProperties.server_allowFishingChore)
 		{
-			if (!MCA.getInstance().modPropertiesManager.modProperties.server_allowFishingChore)
-			{
-				//End the chore and sync all clients so that the chore is stopped everywhere.
-				endChore();
-				PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createSyncPacket(owner));
-				owner.worldObj.getPlayerEntityByName(owner.lastInteractingPlayer).addChatMessage("\u00a7cChore disabled by the server administrator.");
-				return;
-			}
+			//End the chore and sync all clients so that the chore is stopped everywhere.
+			endChore();
+
+			PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createSyncPacket(owner));
+			owner.worldObj.getPlayerEntityByName(owner.lastInteractingPlayer).addChatMessage("\u00a7cChore disabled by the server administrator.");
+			return;
 		}
 
 		owner.isFollowing = false;
@@ -101,202 +99,58 @@ public class ChoreFishing extends AbstractChore
 	@Override
 	public void runChoreAI() 
 	{
-		//Make sure they have a fishing rod.
-		if (owner instanceof EntityPlayerChild)
+		doItemVerification();
+
+		if (hasWaterCoordinates)
 		{
-			if (owner.inventory.getQuantityOfItem(Item.fishingRod) == 0)
+			if (canFishingBegin())
 			{
-				if (!owner.worldObj.isRemote)
-				{
-					owner.say(LanguageHelper.getString("notify.child.chore.interrupted.fishing.norod"));
-				}
-				
-				endChore();
-				return;
-			}
-		}
-
-		//Get all water up to 10 blocks away from the entity.
-		Coordinates waterCoordinates = LogicHelper.getNearbyBlockTopBottom(owner, Block.waterStill.blockID, 10);
-
-		//Check if they don't have some water.
-		if (!hasWaterCoordinates)
-		{
-			//Make sure that AI actually returned some water coordinates.
-			if (waterCoordinates != null)
-			{
-				waterCoordinatesX = (int)waterCoordinates.x;
-				waterCoordinatesY = (int)waterCoordinates.y;
-				waterCoordinatesZ = (int)waterCoordinates.z;
-				hasWaterCoordinates = true;
-			}
-
-			//If it didn't, there's no water around so the chore must end.
-			else
-			{
-				if (!owner.worldObj.isRemote)
-				{
-					owner.say(LanguageHelper.getString("notify.child.chore.interrupted.fishing.nowater"));
-				}
-				
-				endChore();
-				return;
-			}
-		}
-
-		//They do have water. Continue.
-		else
-		{
-			//Check if they are not within 1 block of still water.
-			if (!LogicHelper.isBlockNearby(owner, Block.waterStill.blockID, 1))
-			{
-				//And set a path to their water coordinates if they aren't.
-				owner.getNavigator().setPath(owner.getNavigator().getPathToXYZ(waterCoordinatesX, waterCoordinatesY, waterCoordinatesZ), Constants.SPEED_WALK);
-			}
-
-			//If they are within 1 block of still water, they can begin fishing.
-			else
-			{
-				//Clear their current path to prevent them from entering the water.
 				owner.getNavigator().clearPathEntity();
 
-				//Check if they don't have a random water block to fish at, server side only. Assume they do client side.
-				if (!hasRandomWaterBlock)
+				if (hasFishingTarget)
 				{
-					if (!owner.worldObj.isRemote)
+					if (idleFishingTime < 20)
 					{
-						Coordinates randomWaterCoordinates = LogicHelper.getRandomNearbyBlockCoordinatesOfType(owner, Block.waterStill.blockID);
-
-						waterCoordinatesX = (int)randomWaterCoordinates.x;
-						waterCoordinatesY = (int)randomWaterCoordinates.y;
-						waterCoordinatesZ = (int)randomWaterCoordinates.z;
+						updateFishingIdle();
 					}
 
-					hasRandomWaterBlock = true;
-				}
-
-				//If they do have a random water block to fish at, begin fishing.
-				else
-				{
-					//Check how long they've been idle. (Rod is not thrown)
-					if (idleFishingTicks < 20)
-					{
-						if (fishEntity != null && !owner.worldObj.isRemote)
-						{
-							fishEntity.setDead();
-						}
-
-						AbstractEntity.faceCoordinates(owner, waterCoordinatesX, waterCoordinatesY, waterCoordinatesZ);
-						idleFishingTicks++;
-					}
-
-					//If they have idled for 20 ticks, throw the rod and continue fishing.
 					else
 					{
-						if (fishEntity != null)
+						faceFishEntity();
+
+						if (fishCatchCheck == 0)
 						{
-							AbstractEntity.faceCoordinates(owner, fishEntity.posX, fishEntity.posY, fishEntity.posZ);
+							generateNextFishCatchCheck();
 						}
 
-						//Check if the chance to catch a fish counter has been reset.
-						if (nextFishCatchChance == 0)
+						//See if they've been fishing long enough to attempt catching a fish.
+						if (fishingTicks >= fishCatchCheck)
 						{
-							if (!owner.worldObj.isRemote)
-							{
-								if (owner instanceof EntityPlayerChild)
-								{
-									owner.damageHeldItem();
-								}
-								
-								nextFishCatchChance = owner.worldObj.rand.nextInt(200) + 200;
-								fishEntity = new EntityChoreFishHook(owner.worldObj, owner);
-								owner.worldObj.spawnEntityInWorld(fishEntity);
-								owner.tasks.taskEntries.clear();
-							}
+							doFishCatchAttempt();
 						}
 
-						//The fish catch chance counter hasn't been reset.
 						else
 						{
-							//See if they've been fishing long enough to attempt catching a fish.
-							if (fishingTicks >= nextFishCatchChance)
-							{
-								if (!owner.worldObj.isRemote)
-								{
-									int i = owner.worldObj.rand.nextInt(10);
-
-									if (i <= 4) //About a 30 percent chance of catching the fish. In this case they did catch it.
-									{
-										owner.inventory.addItemStackToInventory(new ItemStack(Item.fishRaw, 1));
-										nextFishCatchChance = 0;
-										fishingTicks = 0;
-
-										//Increment achievement values and check for achievement.
-										if (owner instanceof EntityPlayerChild)
-										{
-											EntityPlayerChild child = (EntityPlayerChild)owner;
-
-											child.fishCaught++;
-
-											if (child.fishCaught >= 100)
-											{
-												EntityPlayer player = child.worldObj.getPlayerEntityByName(child.ownerPlayerName);
-
-												if (player != null)
-												{
-													player.triggerAchievement(MCA.getInstance().achievementChildFish);
-												}
-											}
-										}
-
-										//Check if they're carrying 64 fish and end the chore if they are.
-										if (owner.inventory.getQuantityOfItem(Item.fishRaw) == 64)
-										{
-											owner.say(LanguageHelper.getString("notify.child.chore.finished.fishing"));
-											endChore();
-											return;
-										}
-
-										//Reset idle ticks and get another random water block.
-										idleFishingTicks = 0;
-										hasRandomWaterBlock = false;
-									}
-
-									//They failed to catch the fish. Reset everything.
-									else
-									{
-										nextFishCatchChance = 0;
-										fishingTicks = 0;
-										idleFishingTicks = 0;
-										hasRandomWaterBlock = false;
-									}
-								}
-							}
-
-							//They have not been fishing long enough to try and catch a fish.
-							else
-							{
-								if (!owner.worldObj.isRemote)
-								{
-									//Check and be sure the hook is still there. It will remove itself if it remains in the ground too long.
-									if (fishEntity != null)
-									{
-										fishingTicks++;
-									}
-
-									else
-									{
-										nextFishCatchChance = 0;
-										fishingTicks = 0;
-										idleFishingTicks = 0;
-										//hasRandomWaterBlock = false;
-									}
-								}
-							}
+							updateFishingTicks();
 						}
 					}
 				}
+
+				else //No fishing target.
+				{
+					setFishingTargetBlock();
+				}
 			}
+
+			else //Not within 1 block of water.
+			{
+				owner.getNavigator().setPath(owner.getNavigator().getPathToXYZ(waterCoordinatesX, waterCoordinatesY, waterCoordinatesZ), Constants.SPEED_WALK);
+			}
+		}
+
+		else //No water coordinates.
+		{
+			setWaterCoordinates();
 		}
 	}
 
@@ -317,15 +171,15 @@ public class ChoreFishing extends AbstractChore
 		fishEntity = null;
 		hasEnded = true;
 
-		if (!owner.worldObj.isRemote)
+		if (owner.worldObj.isRemote)
 		{
-			PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createSyncPacket(owner));
-			PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createAddAIPacket(owner));
+			PacketDispatcher.sendPacketToServer(PacketHelper.createAddAIPacket(owner));
 		}
 
 		else
 		{
-			PacketDispatcher.sendPacketToServer(PacketHelper.createAddAIPacket(owner));
+			PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createSyncPacket(owner));
+			PacketDispatcher.sendPacketToAllPlayers(PacketHelper.createAddAIPacket(owner));
 		}
 
 		owner.addAI();
@@ -335,35 +189,35 @@ public class ChoreFishing extends AbstractChore
 	public void writeChoreToNBT(NBTTagCompound NBT) 
 	{
 		//Loop through each field in this class and write to NBT.
-		for (Field f : this.getClass().getFields())
+		for (final Field field : this.getClass().getFields())
 		{
 			try
 			{
-				if (f.getModifiers() != Modifier.TRANSIENT)
+				if (field.getModifiers() != Modifier.TRANSIENT)
 				{
-					if (f.getType().toString().contains("int"))
+					if (field.getType().toString().contains("int"))
 					{
-						NBT.setInteger(f.getName(), (Integer)f.get(owner.fishingChore));
+						NBT.setInteger(field.getName(), (Integer)field.get(owner.fishingChore));
 					}
 
-					else if (f.getType().toString().contains("double"))
+					else if (field.getType().toString().contains("double"))
 					{
-						NBT.setDouble(f.getName(), (Double)f.get(owner.fishingChore));
+						NBT.setDouble(field.getName(), (Double)field.get(owner.fishingChore));
 					}
 
-					else if (f.getType().toString().contains("float"))
+					else if (field.getType().toString().contains("float"))
 					{
-						NBT.setFloat(f.getName(), (Float)f.get(owner.fishingChore));
+						NBT.setFloat(field.getName(), (Float)field.get(owner.fishingChore));
 					}
 
-					else if (f.getType().toString().contains("String"))
+					else if (field.getType().toString().contains("String"))
 					{
-						NBT.setString(f.getName(), (String)f.get(owner.fishingChore));
+						NBT.setString(field.getName(), (String)field.get(owner.fishingChore));
 					}
 
-					else if (f.getType().toString().contains("boolean"))
+					else if (field.getType().toString().contains("boolean"))
 					{
-						NBT.setBoolean(f.getName(), (Boolean)f.get(owner.fishingChore));
+						NBT.setBoolean(field.getName(), (Boolean)field.get(owner.fishingChore));
 					}
 				}
 			}
@@ -380,35 +234,35 @@ public class ChoreFishing extends AbstractChore
 	public void readChoreFromNBT(NBTTagCompound NBT) 
 	{
 		//Loop through each field in this class and read from NBT.
-		for (Field f : this.getClass().getFields())
+		for (final Field field : this.getClass().getFields())
 		{
 			try
 			{
-				if (f.getModifiers() != Modifier.TRANSIENT)
+				if (field.getModifiers() != Modifier.TRANSIENT)
 				{
-					if (f.getType().toString().contains("int"))
+					if (field.getType().toString().contains("int"))
 					{
-						f.set(owner.fishingChore, NBT.getInteger(f.getName()));
+						field.set(owner.fishingChore, NBT.getInteger(field.getName()));
 					}
 
-					else if (f.getType().toString().contains("double"))
+					else if (field.getType().toString().contains("double"))
 					{
-						f.set(owner.fishingChore, NBT.getDouble(f.getName()));
+						field.set(owner.fishingChore, NBT.getDouble(field.getName()));
 					}
 
-					else if (f.getType().toString().contains("float"))
+					else if (field.getType().toString().contains("float"))
 					{
-						f.set(owner.fishingChore, NBT.getFloat(f.getName()));
+						field.set(owner.fishingChore, NBT.getFloat(field.getName()));
 					}
 
-					else if (f.getType().toString().contains("String"))
+					else if (field.getType().toString().contains("String"))
 					{
-						f.set(owner.fishingChore, NBT.getString(f.getName()));
+						field.set(owner.fishingChore, NBT.getString(field.getName()));
 					}
 
-					else if (f.getType().toString().contains("boolean"))
+					else if (field.getType().toString().contains("boolean"))
 					{
-						f.set(owner.fishingChore, NBT.getBoolean(f.getName()));
+						field.set(owner.fishingChore, NBT.getBoolean(field.getName()));
 					}
 				}
 			}
@@ -418,6 +272,174 @@ public class ChoreFishing extends AbstractChore
 				MCA.getInstance().log(e);
 				continue;
 			}
+		}
+	}
+
+	private boolean setWaterCoordinates()
+	{
+		//Get all water up to 10 blocks away from the entity.
+		final Coordinates waterCoordinates = LogicHelper.getNearbyBlockTopBottom(owner, Block.waterStill.blockID, 10);
+
+		if (waterCoordinates == null)
+		{
+			if (!owner.worldObj.isRemote)
+			{
+				owner.say(LanguageHelper.getString("notify.child.chore.interrupted.fishing.nowater"));
+			}
+
+			endChore();			
+			return false;
+		}
+
+		else
+		{
+			waterCoordinatesX = (int)waterCoordinates.x;
+			waterCoordinatesY = (int)waterCoordinates.y;
+			waterCoordinatesZ = (int)waterCoordinates.z;
+			hasWaterCoordinates = true;
+
+			return true;
+		}
+	}
+
+	private boolean canFishingBegin()
+	{
+		return LogicHelper.isBlockNearby(owner, Block.waterStill.blockID, 1);
+	}
+
+	private void setFishingTargetBlock()
+	{
+		if (!owner.worldObj.isRemote)
+		{
+			final Coordinates randomNearbyWater = LogicHelper.getRandomNearbyBlockCoordinatesOfType(owner, Block.waterStill.blockID);
+
+			waterCoordinatesX = (int)randomNearbyWater.x;
+			waterCoordinatesY = (int)randomNearbyWater.y;
+			waterCoordinatesZ = (int)randomNearbyWater.z;
+		}
+
+		hasFishingTarget = true;
+	}
+
+	private void updateFishingIdle()
+	{
+		if (fishEntity != null && !owner.worldObj.isRemote)
+		{
+			fishEntity.setDead();
+		}
+
+		AbstractEntity.faceCoordinates(owner, waterCoordinatesX, waterCoordinatesY, waterCoordinatesZ);
+		idleFishingTime++;
+	}
+
+	private void generateNextFishCatchCheck()
+	{
+		if (!owner.worldObj.isRemote)
+		{
+			if (owner instanceof EntityPlayerChild)
+			{
+				owner.damageHeldItem();
+			}
+
+			fishCatchCheck = owner.worldObj.rand.nextInt(200) + 200;
+			fishEntity = new EntityChoreFishHook(owner.worldObj, owner);
+			owner.worldObj.spawnEntityInWorld(fishEntity);
+			owner.tasks.taskEntries.clear();
+		}
+	}
+
+	private void doFishCatchAttempt()
+	{
+		if (!owner.worldObj.isRemote)
+		{
+			final int catchChance = owner.worldObj.rand.nextInt(10);
+
+			if (catchChance <= 4) //About a 30 percent chance of catching the fish. In this case they did catch it.
+			{
+				owner.inventory.addItemStackToInventory(new ItemStack(Item.fishRaw, 1));
+				fishCatchCheck = 0;
+				fishingTicks = 0;
+
+				//Increment achievement values and check for achievement.
+				if (owner instanceof EntityPlayerChild)
+				{
+					EntityPlayerChild child = (EntityPlayerChild)owner;
+
+					child.fishCaught++;
+
+					if (child.fishCaught >= 100)
+					{
+						final EntityPlayer player = child.worldObj.getPlayerEntityByName(child.ownerPlayerName);
+
+						if (player != null)
+						{
+							player.triggerAchievement(MCA.getInstance().achievementChildFish);
+						}
+					}
+				}
+
+				//Check if they're carrying 64 fish and end the chore if they are.
+				if (owner.inventory.getQuantityOfItem(Item.fishRaw) == 64)
+				{
+					owner.say(LanguageHelper.getString("notify.child.chore.finished.fishing"));
+					endChore();
+					return;
+				}
+
+				//Reset idle ticks and get another random water block.
+				idleFishingTime = 0;
+				hasFishingTarget = false;
+			}
+
+			//They failed to catch the fish. Reset everything.
+			else
+			{
+				fishCatchCheck = 0;
+				fishingTicks = 0;
+				idleFishingTime = 0;
+				hasFishingTarget = false;
+			}
+		}
+	}
+
+	private void updateFishingTicks()
+	{
+		if (!owner.worldObj.isRemote)
+		{
+			if (fishEntity == null)
+			{
+				fishCatchCheck = 0;
+				fishingTicks = 0;
+				idleFishingTime = 0;
+			}
+
+			else
+			{
+				fishingTicks++;
+			}
+		}
+	}
+
+	private void faceFishEntity()
+	{
+		if (fishEntity != null)
+		{
+			AbstractEntity.faceCoordinates(owner, fishEntity.posX, fishEntity.posY, fishEntity.posZ);
+		}
+	}
+
+	private void doItemVerification()
+	{
+		//Make sure a child has a fishing rod.
+		if (owner instanceof EntityPlayerChild && owner.inventory.getQuantityOfItem(Item.fishingRod) == 0)
+		{
+			if (!owner.worldObj.isRemote)
+			{
+				owner.say(LanguageHelper.getString("notify.child.chore.interrupted.fishing.norod"));
+			}
+
+			endChore();
+			return;
 		}
 	}
 }
