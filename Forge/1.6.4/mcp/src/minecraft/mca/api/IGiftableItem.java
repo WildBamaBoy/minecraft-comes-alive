794433d4f351d28b4a80e411ff819e943a84b6f6
WildBamaBoy <dustya@otelco.net>
2013-12-30 17:07:15 -0600

    Documentation update

--- Forge/1.6.4/mcp/src/minecraft/mca/api/IGiftableItem.java
+++ Forge/1.6.4/mcp/src/minecraft/mca/api/IGiftableItem.java
@@ -21,7 +21,7 @@ public interface IGiftableItem
 	 * they will also equip items that extend from ItemHoe or ItemFishingRod.
 	 * 
 	 * If you implement this interface on an item that extends from any of the mentioned classes, the item will 
-	 * be treated as a regular gift and disappear permanently while increasing a villagers' hearts.
+	 * be treated as a regular gift instead and disappear permanently while increasing a villagers' hearts.
 	 * 
 	 * @return	Base amount that hearts will increase when item is gifted. This is modified by a villager's 
 	 * 			interaction fatigue, mood, and trait.
