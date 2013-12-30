23e4459d404e587dd554d621b3fa754e5c298925
WildBamaBoy <dustya@otelco.net>
2013-12-30 06:24:03 -0600

    Refactored renderers

--- Forge/1.6.4/mcp/src/minecraft/mca/client/render/RenderTombstone.java
+++ Forge/1.6.4/mcp/src/minecraft/mca/client/render/RenderTombstone.java
@@ -10,6 +10,7 @@
 package mca.client.render;
 
 import mca.client.model.ModelTombstone;
+import mca.core.Constants;
 import mca.tileentity.TileEntityTombstone;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.texture.TextureManager;
@@ -25,9 +26,7 @@ import org.lwjgl.opengl.GL11;
 public class RenderTombstone extends TileEntitySpecialRenderer
 {
 	private static final ResourceLocation texture = new ResourceLocation("mca:textures/blocks/Tombstone.png");
-	
-	/** The model of the tombstone. */
-	private ModelTombstone tombstoneModel;
+	private final ModelTombstone tombstoneModel;
 
 	/**
 	 * Constructor
@@ -48,36 +47,48 @@ public class RenderTombstone extends TileEntitySpecialRenderer
 	 */
 	public void renderTileEntityTombstoneAt(TileEntityTombstone tombstoneEntity, double posX, double posY, double posZ, float partialTickTime)
 	{
-		int meta = tombstoneEntity.getBlockMetadata();
+		final StringBuilder stringBuilder = new StringBuilder();
+		final FontRenderer fontRenderer = getFontRenderer();
+		fontRenderer.FONT_HEIGHT = 4;
+
+		final int meta = tombstoneEntity.getBlockMetadata();
 		float rotation = 0.0F;
-		
+
 		GL11.glPushMatrix();
 
 		switch (meta)
 		{
-			case 2: rotation = 180F; break;
-			case 4: rotation = 90F; break;
-			case 8: rotation = -180F; break;
-			case 12: rotation = -90F; break;
+		case 0: rotation = 0.0F; break;
+		case 1: rotation = 45F; break;
+		case 2: rotation = 45F; break;
+		case 3: rotation = 45F; break;
+		case 4: rotation = 90F; break;
+		case 5: rotation = 135F; break;
+		case 6: rotation = 135F; break;
+		case 7: rotation = 135F; break;
+		case 8: rotation = 180F; break;
+		case 9: rotation = 225F; break;
+		case 10: rotation = 225F; break;
+		case 11: rotation = 225F; break;
+		case 12: rotation = 270F; break;
+		case 13: rotation = 315F; break;
+		case 14: rotation = 315F; break;
+		case 15: rotation = 315F; break;
+		default: break;
 		}
 
-		//Orient the tombstone properly and give it a texture.
-		GL11.glTranslatef((float)posX + 0.5F, (float)posY + 1F, (float)posZ + 0.5F);
+		GL11.glTranslated(posX + 0.45F, posY + 1F, posZ + 0.53F);
 		GL11.glRotatef(-rotation, 0.0F, 1.0F, 0.0F);
 		GL11.glTranslatef(0, 0.5F, 0);
-		this.func_110628_a(texture);
+		this.bindResource(texture);
 
 		GL11.glPushMatrix();
-		
-		GL11.glScalef(1F, -1F, -1F);
-		tombstoneModel.renderTombstone();
-		
+		{
+			GL11.glScalef(1F, -1F, -1F);
+			tombstoneModel.renderTombstone();
+		}
 		GL11.glPopMatrix();
-		
-		//Render the text.
-		FontRenderer fontrenderer = getFontRenderer();
-		fontrenderer.FONT_HEIGHT = 4;
-		
+
 		//Text size is 0.017F.
 		GL11.glTranslatef(0.0F, -1.1F, 0.07F);
 		GL11.glScalef(0.017F / 2, -0.017F / 2, 0.017F / 2);
@@ -86,40 +97,17 @@ public class RenderTombstone extends TileEntitySpecialRenderer
 
 		for (int line = 0; line < tombstoneEntity.signText.length; line++)
 		{
-			String lineText = tombstoneEntity.signText[line];
+			String lineText = Constants.FORMAT_BOLD + tombstoneEntity.signText[line];
 
 			if (line == tombstoneEntity.lineBeingEdited)
 			{
-				lineText = (new StringBuilder()).append("> ").append(lineText).append(" <").toString();
-
-				if (tombstoneEntity.guiOpen)
-				{
-					fontrenderer.drawString(lineText, -fontrenderer.getStringWidth(lineText) / 2, line * 10 - tombstoneEntity.signText.length * 5, 0x000000);
-				}
-
-				else
-				{
-					fontrenderer.drawString(lineText, -fontrenderer.getStringWidth(lineText) / 2, line * 10 - tombstoneEntity.signText.length * 5, 0xCFB52B);
-				}
+				lineText = stringBuilder.append("> ").append(lineText).append(" <").toString();
 			}
 			
-			else
-			{
-				if (tombstoneEntity.guiOpen)
-				{
-					fontrenderer.drawString(lineText, -fontrenderer.getStringWidth(lineText) / 2, line * 10 - tombstoneEntity.signText.length * 5, 0x000000);
-				}
-				
-				else
-				{
-					fontrenderer.drawString(lineText, -fontrenderer.getStringWidth(lineText) / 2, line * 10 - tombstoneEntity.signText.length * 5, 0xCFB52B);
-				}
-			}
-
+			fontRenderer.drawString(lineText, -fontRenderer.getStringWidth(lineText) / 2, line * 10 - tombstoneEntity.signText.length * 5, 0x000000);
 		}
 
 		GL11.glDepthMask(true);
-		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		GL11.glPopMatrix();
 	}
 
@@ -128,14 +116,14 @@ public class RenderTombstone extends TileEntitySpecialRenderer
 	{
 		renderTileEntityTombstoneAt((TileEntityTombstone)tombstoneEntity, posX, posY, posZ, partialTickTime);
 	}
-	
-    protected void func_110628_a(ResourceLocation resourceLocation)
-    {
-        TextureManager texturemanager = this.tileEntityRenderer.renderEngine;
 
-        if (texturemanager != null)
-        {
-            texturemanager.bindTexture(resourceLocation);
-        }
-    }
+	protected void bindResource(ResourceLocation resourceLocation)
+	{
+		final TextureManager textureManager = this.tileEntityRenderer.renderEngine;
+
+		if (textureManager != null)
+		{
+			textureManager.bindTexture(resourceLocation);
+		}
+	}
 }
