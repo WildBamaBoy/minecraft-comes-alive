ec2860a430b78c3f3fcf1c611df36daf79dab262
Marty Plummer <ntzrmtthihu777@gmail.com>
2014-09-08 03:11:51 -0500

    added copyright header

--- src/main/java/mca/network/packets/TypeIDs.java
+++ src/main/java/mca/network/packets/TypeIDs.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * TypeIDs.java
+ * Copyright (c) 2014 Radix-Shock Entertainment.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MCA Minecraft Mod license.
+ ******************************************************************************/
+
 package mca.network.packets;
 
 public final class TypeIDs
