005f1ebde7ede54b2eaefbedb13bf6d0dbc0baf6
WildBamaBoy <dusty.s.alexander@gmail.com>
2015-02-16 00:23:47 -0600

    Refresh current location if no destiny choice is made

--- src/main/java/mca/packets/PacketDestinyChoice.java
+++ src/main/java/mca/packets/PacketDestinyChoice.java
@@ -1,27 +1,24 @@
 package mca.packets;
 
-import java.io.IOException;
-import java.util.Map;
-
 import io.netty.buffer.ByteBuf;
-import mca.core.MCA;
 import mca.enums.EnumDestinyChoice;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.management.PlayerManager;
+import net.minecraft.world.WorldServer;
 import net.minecraft.world.biome.BiomeGenOcean;
 import net.minecraft.world.chunk.Chunk;
-import radixcore.data.BlockWithMeta;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import radixcore.helpers.LogicHelper;
 import radixcore.helpers.MathHelper;
 import radixcore.math.Point3D;
 import radixcore.packets.AbstractPacket;
-import radixcore.util.SchematicReader;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 
 public class PacketDestinyChoice extends AbstractPacket implements IMessage, IMessageHandler<PacketDestinyChoice, IMessage>
 {
-	private int choice;
+	private EnumDestinyChoice choice;
 
 	public PacketDestinyChoice()
 	{
@@ -29,40 +26,64 @@ public class PacketDestinyChoice extends AbstractPacket implements IMessage, IMe
 
 	public PacketDestinyChoice(EnumDestinyChoice choice)
 	{
-		this.choice = choice.getId();
+		this.choice = choice;
 	}
 
 	@Override
 	public void fromBytes(ByteBuf byteBuf)
 	{
-		choice = byteBuf.readInt();
+		choice = EnumDestinyChoice.fromId(byteBuf.readInt());
 	}
 
 	@Override
 	public void toBytes(ByteBuf byteBuf)
 	{
-		byteBuf.writeInt(choice);
+		byteBuf.writeInt(choice.getId());
 	}
 
 	@Override
 	public IMessage onMessage(PacketDestinyChoice packet, MessageContext context)
 	{
-		EntityPlayer senderPlayer = this.getPlayer(context);
-
-		Chunk newSpawnChunk = null;
-		int x, y, z = 0;
-
-		do
+		final EntityPlayerMP senderPlayer = (EntityPlayerMP)this.getPlayer(context);
+		final WorldServer world = (WorldServer)senderPlayer.worldObj;
+		
+		if (packet.choice == EnumDestinyChoice.NONE)
 		{
-			x = (int) (MathHelper.getNumberInRange(-4096, 4096) + (senderPlayer.posX / 2));
-			z = (int) (MathHelper.getNumberInRange(-4096, 4096) + (senderPlayer.posZ / 2));
-			y = LogicHelper.getSpawnSafeTopLevel(senderPlayer.worldObj, x, z);
-
-			newSpawnChunk = senderPlayer.worldObj.getChunkFromChunkCoords(x >> 4, z >> 4);
+			//Update the region around the player so that the destiny room disappears.
+			final PlayerManager manager = world.getPlayerManager();
+			
+			for (int x = -20; x < 20; x++)
+			{
+				for (int z = -20; z < 20; z++)
+				{
+					for (int y = -5; y < 10; y++)
+					{
+						manager.markBlockForUpdate((int)senderPlayer.posX + x, (int)senderPlayer.posY + y, (int)senderPlayer.posZ + z);						
+					}
+				}
+			}
 		}
-		while (newSpawnChunk.getBiomeGenForWorldCoords(x & 15, z & 13, senderPlayer.worldObj.getWorldChunkManager()) instanceof BiomeGenOcean);
 
-		senderPlayer.setPositionAndUpdate(x, y, z);
+		else
+		{
+			Chunk newSpawnChunk = null;
+			int x, y, z = 0;
+
+			do
+			{
+				x = (int) (MathHelper.getNumberInRange(-4096, 4096) + (senderPlayer.posX / 2));
+				z = (int) (MathHelper.getNumberInRange(-4096, 4096) + (senderPlayer.posZ / 2));
+				y = LogicHelper.getSpawnSafeTopLevel(senderPlayer.worldObj, x, z);
+
+				newSpawnChunk = senderPlayer.worldObj.getChunkFromChunkCoords(x >> 4, z >> 4);
+			}
+			while (newSpawnChunk.getBiomeGenForWorldCoords(x & 15, z & 13, senderPlayer.worldObj.getWorldChunkManager()) instanceof BiomeGenOcean);
+
+			final Point3D oldPoint = new Point3D(senderPlayer.posX, senderPlayer.posY, senderPlayer.posZ);
+			senderPlayer.setPositionAndUpdate(x, y, z);
+			
+			//TODO Build village/generate required elements for destiny choice.
+		}
 
 		return null;
 	}
