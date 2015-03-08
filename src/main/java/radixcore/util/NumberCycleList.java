328b2656d85ec252bb6fc9756589a9a8efb26a57
WildBamaBoy <dusty.s.alexander@gmail.com>
2015-03-08 00:47:34 -0600

    Add NumberCycleList

---
+++ src/main/java/radixcore/util/NumberCycleList.java
@@ -0,0 +1,58 @@
+package radixcore.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Creates a list that can be cycled through in order using next() and previous(). Does not permit duplicates or null values.
+ */
+public final class NumberCycleList extends ArrayList<Integer>
+{
+	private int index;
+	
+	public static NumberCycleList fromList(List<Integer> list)
+	{
+		NumberCycleList returnList = new NumberCycleList();
+		
+		for (Integer i : list)
+		{
+			if (!returnList.contains(i) && i != null)
+			{
+				returnList.add(i);
+			}
+		}
+		
+		Collections.sort(returnList);
+		return returnList;
+	}
+	
+	public int next()
+	{
+		index++;
+
+		if (index > size() - 1)
+		{
+			index = 0;
+		}
+		
+		return get(index);
+	}
+	
+	public int previous()
+	{
+		index--;
+		
+		if (index < 0)
+		{
+			index = size() - 1;
+		}
+		
+		return get(index);
+	}
+	
+	public int get()
+	{
+		return get(index);
+	}
+}
