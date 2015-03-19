9c9783c92040cb3c61fdb098ce24f7f0d699e44c
WildBamaBoy <dusty.s.alexander@gmail.com>
2015-03-19 16:29:14 -0500

    Make crash watcher function properly

--- src/main/java/mca/core/radix/CrashWatcher.java
+++ src/main/java/mca/core/radix/CrashWatcher.java
@@ -1,8 +1,13 @@
 package mca.core.radix;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.net.Socket;
 import java.util.Scanner;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import mca.core.Constants;
 import mca.core.MCA;
 import radixcore.core.ModCrashWatcher;
 
@@ -13,13 +18,21 @@ public class CrashWatcher extends ModCrashWatcher
 	{
 		try
 		{
-			Scanner scanner = new Scanner(crashFile);
-			String fileContent = scanner.useDelimiter("\\Z").next();
-
-			MCA.getLog().fatal("Crash detected!");
-			//RDXServerBridge.sendCrashReport(fileContent, true); //TODO
+			String report = new Scanner(crashFile).useDelimiter("\\Z").next();
+			boolean isServer = FMLCommonHandler.instance().getEffectiveSide().isServer();
 			
-			scanner.close();
+			if (report.contains("at mca."))
+			{
+				final Socket connectSocket = new Socket("vps.radix-shock.com", 3577);
+				final DataOutputStream dataOut = new DataOutputStream(connectSocket.getOutputStream());
+				new DataInputStream(connectSocket.getInputStream());
+				dataOut.writeByte(2);
+				dataOut.writeUTF("MCA");
+				dataOut.writeUTF(MCA.VERSION);
+				dataOut.writeBoolean(isServer);
+				dataOut.writeUTF(report);
+				connectSocket.close();
+			}
 		}
 
 		catch (Exception e)
