9acff6df512f374fb463d4a1c0a9741646049b8b
WildBamaBoy <wildbamaboy@gmail.com>
2015-07-11 21:41:01 -0500

    Fix #413 and #422 on 1.7.10

--- src/main/java/mca/core/MCA.java
+++ src/main/java/mca/core/MCA.java
@@ -471,6 +471,9 @@ public class MCA
 		RegistryMCA.addObjectAsGift(Blocks.piston, 10);
 		RegistryMCA.addObjectAsGift(Blocks.glowstone, 10);
 		RegistryMCA.addObjectAsGift(Blocks.emerald_block, 100);
+		RegistryMCA.addObjectAsGift(ModBlocks.roseGoldBlock, 35);
+		RegistryMCA.addObjectAsGift(ModBlocks.roseGoldOre, 7);
+		RegistryMCA.addObjectAsGift(Blocks.redstone_block, 20);
 		
 		RegistryMCA.addBlockToMiningAI(1, Blocks.coal_ore);
 		RegistryMCA.addBlockToMiningAI(2, Blocks.iron_ore);
