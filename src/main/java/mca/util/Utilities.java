b7b6e609eca036a9c22dae74d7b386715f5a1597
WildBamaBoy <wildbamaboy@gmail.com>
2015-08-01 15:05:39 -0500

    Change: Tweaked height and girth distribution per issue #288.

--- src/main/java/mca/util/Utilities.java
+++ src/main/java/mca/util/Utilities.java
@@ -1,5 +1,7 @@
 package mca.util;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import radixcore.util.BlockHelper;
@@ -15,5 +17,10 @@ public class Utilities
 	{
 		Block block = BlockHelper.getBlock(world, posX, posY, posZ);
 		return !block.getMaterial().blocksMovement();
+	}
+	
+	public static double getNumberInRange(Random rand, float standardDeviation, float mean)
+	{
+		return (rand.nextGaussian() * standardDeviation) + mean;
 	}
 }
