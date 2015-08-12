778a7ae07ba6a314751c8c908c6b43cff64c409b
WildBamaBoy <wildbamaboy@gmail.com>
2015-08-11 19:02:19 -0500

    Fix: Potential crash when children go to sleep has been resolved.

--- src/main/java/mca/entity/EntityHuman.java
+++ src/main/java/mca/entity/EntityHuman.java
@@ -1307,8 +1307,17 @@ public class EntityHuman extends EntityVillager implements IWatchable, IPermanen
 
 	public boolean isPlayerAParent(EntityPlayer player)
 	{
-		final PlayerData data = MCA.getPlayerData(player);	
-		return getMotherId() == data.permanentId.getInt() || getFatherId() == data.permanentId.getInt();
+		final PlayerData data = MCA.getPlayerData(player);
+		
+		if (data != null)
+		{
+			return getMotherId() == data.permanentId.getInt() || getFatherId() == data.permanentId.getInt();
+		}
+		
+		else
+		{
+			return false;
+		}
 	}
 
 	public boolean allowControllingInteractions(EntityPlayer player)
