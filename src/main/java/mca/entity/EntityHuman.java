bfd9c05939bda2d85acf7e17ad95d146ae769501
WildBamaBoy <wildbamaboy@gmail.com>
2016-03-19 20:15:17 -0500

    Fix (1.7.10): Married guards should no longer patrol at night. #610

--- src/main/java/mca/entity/EntityHuman.java
+++ src/main/java/mca/entity/EntityHuman.java
@@ -276,7 +276,7 @@ public class EntityHuman extends EntityVillager implements IWatchable, IPermanen
 			this.setHealth(maxHealth);
 		}
 
-		if (this.getProfessionGroup() != EnumProfessionGroup.Guard)
+		if (this.getProfessionGroup() != EnumProfessionGroup.Guard || (this.getProfessionGroup() == EnumProfessionGroup.Guard && this.getIsMarried()))
 		{
 			this.tasks.addTask(2, new EntityAIMoveIndoors(this));
 		}
@@ -919,6 +919,9 @@ public class EntityHuman extends EntityVillager implements IWatchable, IPermanen
 
 			getAI(AIProgressStory.class).reset();
 		}
+		
+		//Reset Minecraft AI when this happens.
+		addAI();
 	}
 
 	public boolean getIsMarried()
