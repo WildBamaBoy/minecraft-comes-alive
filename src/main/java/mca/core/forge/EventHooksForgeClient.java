0d0f255520b62e40b617f3d766cd6ed1bd4a8faf
WildBamaBoy <wildbamaboy@gmail.com>
2016-04-16 18:33:56 -0500

    Fix crystal ball usage

--- src/main/java/mca/core/forge/EventHooksForgeClient.java
+++ src/main/java/mca/core/forge/EventHooksForgeClient.java
@@ -6,6 +6,7 @@ import mca.data.PlayerData;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEnchantmentTable;
 import net.minecraft.client.Minecraft;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import radixcore.util.BlockHelper;
@@ -17,12 +18,12 @@ public class EventHooksForgeClient
 	{
 		if (event.getEntityPlayer().worldObj.isRemote && Minecraft.getMinecraft().isIntegratedServerRunning())
 		{
-			Block block = BlockHelper.getBlock(event.getWorld(), (int)event.getHitVec().xCoord, (int)event.getHitVec().yCoord, (int)event.getHitVec().zCoord);
+			Block block = BlockHelper.getBlock(event.getWorld(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ());
 
-			if (block instanceof BlockEnchantmentTable)
+			if (block == Blocks.enchanting_table)
 			{
 				PlayerData data = MCA.getPlayerData(event.getEntityPlayer());
-
+				
 				if (!data.getHasChosenDestiny())
 				{
 					event.setCanceled(true);
