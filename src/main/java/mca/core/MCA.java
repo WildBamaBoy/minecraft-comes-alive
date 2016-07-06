475042e6068e0db6ebd2ae45a2c6ae88879c594e
WildBamaBoy <wildbamaboy@gmail.com>
2016-07-05 23:44:55 -0500

    Fix startup issues

--- src/main/java/mca/core/MCA.java
+++ src/main/java/mca/core/MCA.java
@@ -85,8 +85,10 @@ import radixcore.util.RadixExcept;
 import radixcore.util.RadixLogic;
 import radixcore.util.RadixStartup;
 
-@Mod(modid = MCA.ID, name = MCA.NAME, version = MCA.VERSION, dependencies = "required-after:RadixCore@[1.9-2.1.2,)", acceptedMinecraftVersions = "[1.8.9]",
+@Mod(modid = MCA.ID, name = MCA.NAME, version = MCA.VERSION, dependencies = "required-after:RadixCore", acceptedMinecraftVersions = "[1.9]",
 guiFactory = "mca.core.forge.client.MCAGuiFactory")
+//@Mod(modid = MCA.ID, name = MCA.NAME, version = MCA.VERSION, dependencies = "required-after:RadixCore@[1.9-2.1.2,)", acceptedMinecraftVersions = "[1.9]",
+//guiFactory = "mca.core.forge.client.MCAGuiFactory")
 public class MCA
 {
 	public static final String ID = "MCA";
