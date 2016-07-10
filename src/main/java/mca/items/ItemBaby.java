5ef01296f017c8aee6350369bdf1c141869cbf7d
WildBamaBoy <wildbamaboy@gmail.com>
2016-07-10 12:43:45 -0500

    Fix: Crash on dropping a baby on multiplayer has been fixed. #741

--- src/main/java/mca/items/ItemBaby.java
+++ src/main/java/mca/items/ItemBaby.java
@@ -160,7 +160,12 @@ public class ItemBaby extends Item
 	@Override
 	public boolean onEntityItemUpdate(EntityItem entityItem) 
 	{
-		updateBabyGrowth(entityItem.getEntityItem());
+		//Happens on servers for some reason.
+		if (entityItem.getEntityItem() != null)
+		{
+			updateBabyGrowth(entityItem.getEntityItem());
+		}
+		
 		return super.onEntityItemUpdate(entityItem);
 	}
 
