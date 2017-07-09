df2791130ba5956791b07fba765d3a23d59d6301
Dusty A <wildbamaboy@gmail.com>
2017-07-09 16:35:34 -0500

    Fix for Forge update

--- src/main/java/mca/core/forge/client/MCAGuiFactory.java
+++ src/main/java/mca/core/forge/client/MCAGuiFactory.java
@@ -31,4 +31,14 @@ public class MCAGuiFactory implements IModGuiFactory
 	{
 		return null;
 	}
+
+	@Override
+	public boolean hasConfigGui() {
+		return true;
+	}
+
+	@Override
+	public GuiScreen createConfigGui(GuiScreen parentScreen) {
+		return new GuiConfigPage(parentScreen);
+	}
 }
