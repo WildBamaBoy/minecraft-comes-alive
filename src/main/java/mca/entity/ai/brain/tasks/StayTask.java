24ba09dbc6f4d4311f38e129ad1907cf00c4ca38
ffrann <45723631+frqnny@users.noreply.github.com>
2021-05-23 20:26:30 -0400

    Attempt to fix StayTask (didn't work )

--- src/main/java/mca/entity/ai/brain/tasks/StayTask.java
+++ src/main/java/mca/entity/ai/brain/tasks/StayTask.java
@@ -3,6 +3,7 @@ package mca.entity.ai.brain.tasks;
 import com.google.common.collect.ImmutableMap;
 import mca.core.minecraft.MemoryModuleTypeMCA;
 import mca.entity.EntityVillagerMCA;
+import net.minecraft.entity.ai.brain.memory.MemoryModuleType;
 import net.minecraft.entity.ai.brain.task.Task;
 import net.minecraft.world.server.ServerWorld;
 
@@ -30,7 +31,8 @@ public class StayTask extends Task<EntityVillagerMCA> {
     @Override
     protected void tick(ServerWorld world, EntityVillagerMCA villager, long p_212833_3_) {
         villager.getNavigation().stop();
-
+        villager.getBrain().eraseMemory(MemoryModuleType.LOOK_TARGET);
+        villager.getBrain().eraseMemory(MemoryModuleType.WALK_TARGET);
 
     }
 }
