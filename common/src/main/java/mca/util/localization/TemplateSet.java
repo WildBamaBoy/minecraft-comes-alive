7c93e4947f585c3c8b94d92b241f81a5a253da21
Sollace <sollacea@gmail.com>
2021-07-28 19:34:49 +0200

    Fixed supporter text and added support for multiple supporter names in one line of dialogue. Specify using %supporter:1%, %supporter:2%, etc...

--- common/src/main/java/mca/util/localization/TemplateSet.java
+++ common/src/main/java/mca/util/localization/TemplateSet.java
@@ -3,6 +3,8 @@ package mca.util.localization;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import mca.resources.API;
 
@@ -17,6 +19,7 @@ class TemplateSet {
     }
 
     public String replace(String input) {
+        input = "%Supporter:1% told me about %Supporter:2%, but %Supporter:2% said %Supporter:1% was a bad guy. What do you think, %s?";
         for (String name : variables.keySet()) {
             input = replaceAll(name, input);
         }
@@ -24,10 +27,23 @@ class TemplateSet {
     }
 
     private String replaceAll(String name, String input) {
-        String variable = "%v" + name + "%";
-        while (input.contains(variable)) {
-            input = input.replaceAll(variable, variables.get(name).get());
+        StringBuffer buffer = new StringBuffer();
+        Matcher matcher = Pattern.compile("\\%" + name + "(\\:[0-9]+)?\\%", Pattern.CASE_INSENSITIVE).matcher(input);
+
+        Map<String, String> computed = new HashMap<>();
+
+        while (matcher.find()) {
+            String found = matcher.group();
+            String replacement = computed.get(found.toLowerCase());
+            if (replacement == null) {
+                replacement = variables.get(name).get();
+                computed.put(found.toLowerCase(), replacement);
+            }
+
+            matcher.appendReplacement(buffer, replacement);
         }
-        return input;
+        matcher.appendTail(buffer);
+
+        return buffer.toString();
     }
 }
