a1067cd4cbf8cc0a5f9c4f5bcb507f158a4b792a
Luke100000 <jonas.peche@aon.at>
2021-10-09 18:17:28 +0200

    extended and fixed admin commands

--- common/src/main/java/mca/server/command/AdminCommand.java
+++ common/src/main/java/mca/server/command/AdminCommand.java
@@ -6,8 +6,11 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import mca.Config;
 import mca.entity.VillagerEntityMCA;
+import mca.entity.ai.relationship.MarriageState;
 import mca.item.BabyItem;
 import mca.server.world.data.PlayerSaveData;
 import mca.server.world.data.Village;
@@ -19,8 +22,6 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Util;
-import java.util.ArrayList;
-import java.util.stream.Stream;
 
 import static net.minecraft.util.Formatting.*;
 
@@ -28,7 +29,6 @@ public class AdminCommand {
     private static final ArrayList<VillagerEntityMCA> prevVillagersRemoved = new ArrayList<>();
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        // TODO: proper command syntax.
         dispatcher.register(CommandManager.literal("mca-admin")
                 .then(register("help", AdminCommand::displayHelp))
                 .then(register("clearLoadedVillagers", AdminCommand::clearLoadedVillagers))
@@ -38,9 +38,8 @@ public class AdminCommand {
                 .then(register("forceChildGrowth", AdminCommand::forceChildGrowth))
                 .then(register("incrementHearts", AdminCommand::incrementHearts))
                 .then(register("decrementHearts", AdminCommand::decrementHearts))
-                //.then(register("dumpPlayerData", CommandMCAAdmin::dumpPlayerData))
-                //.then(register("resetVillagerData", CommandMCAAdmin::resetVillagerData))
                 .then(register("resetPlayerData", AdminCommand::resetPlayerData))
+                .then(register("resetMarriage", AdminCommand::resetMarriage))
                 .then(register("listVillages", AdminCommand::listVillages))
                 .then(register("removeVillage").then(CommandManager.argument("id", IntegerArgumentType.integer()).executes(AdminCommand::removeVillage)))
                 .then(register("buildingProcessingRate").then(CommandManager.argument("cooldown", IntegerArgumentType.integer()).executes(AdminCommand::buildingProcessingRate)))
@@ -65,7 +64,7 @@ public class AdminCommand {
         if (VillageManager.get(ctx.getSource().getWorld()).removeVillage(id)) {
             success("Village deleted.", ctx);
         } else {
-            fail(ctx);
+            fail("Village with this ID does not exist.", ctx);
         }
         return 0;
     }
@@ -80,6 +79,15 @@ public class AdminCommand {
         PlayerEntity player = ctx.getSource().getPlayer();
         PlayerSaveData playerData = PlayerSaveData.get(ctx.getSource().getWorld(), player.getUuid());
         playerData.reset();
+        success("Player data reset.", ctx);
+        return 0;
+    }
+
+    private static int resetMarriage(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+        PlayerEntity player = ctx.getSource().getPlayer();
+        PlayerSaveData playerData = PlayerSaveData.get(ctx.getSource().getWorld(), player.getUuid());
+        playerData.endMarriage(MarriageState.SINGLE);
+        success("Marriage reset.", ctx);
         return 0;
     }
 
@@ -105,7 +113,10 @@ public class AdminCommand {
         ItemStack heldStack = player.getMainHandStack();
 
         if (heldStack.getItem() instanceof BabyItem) {
-            //((ItemBaby) heldStack.getItem()).forceAgeUp();
+            heldStack.getOrCreateTag().putInt("age", Config.getInstance().babyGrowUpTime);
+            success("Baby is old enough to place now.", ctx);
+        } else {
+            fail("Hold a baby first.", ctx);
         }
         return 0;
     }
@@ -117,6 +128,10 @@ public class AdminCommand {
     }
 
     private static int restoreClearedVillagers(CommandContext<ServerCommandSource> ctx) {
+        for (VillagerEntityMCA v : prevVillagersRemoved) {
+            v.removed = false;
+            ctx.getSource().getWorld().spawnEntity(v);
+        }
         prevVillagersRemoved.clear();
         success("Restored cleared villagers.", ctx);
         return 0;
@@ -149,18 +164,21 @@ public class AdminCommand {
         ctx.getSource().sendFeedback(new LiteralText(message).formatted(GREEN), true);
     }
 
-    private static void fail(CommandContext<ServerCommandSource> ctx) {
-        ctx.getSource().sendError(new LiteralText("Village with this ID does not exist.").formatted(RED));
+    private static void fail(String message, CommandContext<ServerCommandSource> ctx) {
+        ctx.getSource().sendError(new LiteralText(message).formatted(RED));
     }
 
-    private static int displayHelp(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+    private static int displayHelp(CommandContext<ServerCommandSource> ctx) {
         Entity player = ctx.getSource().getEntity();
+        if (player == null) {
+            return 0;
+        }
 
         sendMessage(player, DARK_RED + "--- " + GOLD + "OP COMMANDS" + DARK_RED + " ---");
         sendMessage(player, WHITE + " /mca-admin forceFullHearts " + GOLD + " - Force all hearts on all villagers.");
         sendMessage(player, WHITE + " /mca-admin forceBabyGrowth " + GOLD + " - Force your baby to grow up.");
         sendMessage(player, WHITE + " /mca-admin forceChildGrowth " + GOLD + " - Force nearby children to grow.");
-        sendMessage(player, WHITE + " /mca-admin clearLoadedVillagers " + GOLD + " - Clear all loaded villagers. " + RED + "(IRREVERSABLE)");
+        sendMessage(player, WHITE + " /mca-admin clearLoadedVillagers " + GOLD + " - Clear all loaded villagers. " + RED + "(IRREVERSIBLE)");
         sendMessage(player, WHITE + " /mca-admin restoreClearedVillagers " + GOLD + " - Restores cleared villagers. ");
 
         sendMessage(player, WHITE + " /mca-admin listVillages " + GOLD + " - Prints a list of all villages.");
@@ -169,13 +187,15 @@ public class AdminCommand {
         sendMessage(player, WHITE + " /mca-admin incrementHearts " + GOLD + " - Increase hearts by 10.");
         sendMessage(player, WHITE + " /mca-admin decrementHearts " + GOLD + " - Decrease hearts by 10.");
         sendMessage(player, WHITE + " /mca-admin cve" + GOLD + " - Remove all villager editors from the game.");
-        sendMessage(player, WHITE + " /mca-admin resetPlayerData " + GOLD + " - Resets hearts, marriage statuc etc.");
+        sendMessage(player, WHITE + " /mca-admin resetPlayerData " + GOLD + " - Resets hearts, marriage status etc.");
+        sendMessage(player, WHITE + " /mca-admin resetMarriage " + GOLD + " - Resets your marriage.");
 
         sendMessage(player, WHITE + " /mca-admin listVillages " + GOLD + " - List all known villages.");
         sendMessage(player, WHITE + " /mca-admin removeVillage " + GOLD + " - Remove a given village.");
 
         sendMessage(player, DARK_RED + "--- " + GOLD + "GLOBAL COMMANDS" + DARK_RED + " ---");
         sendMessage(player, WHITE + " /mca-admin help " + GOLD + " - Shows this list of commands.");
+
         return 0;
     }
 
